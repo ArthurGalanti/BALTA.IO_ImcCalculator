e6e7bae8ef0518e3df1d1dad06cf1a59009ef7a7
ArthurGalanti <arthur.galanti@hotmail.com>
2023-04-03 17:15:08 -0300

    Adicionado retorno de grau de obesidade

--- ImcCalculator.Tests/ImcCalculator.cs
+++ ImcCalculator.Tests/ImcCalculator.cs
@@ -7,7 +7,7 @@ public class MoneyExtensionTests
     public void ShouldCalculateIMC()
     {
         var imc = Calculator.CalculateIMC(1.80,66.0);
-        var imcWaited = (20.37, "Peso normal");
+        var imcWaited = (20.37, "Peso normal",0);
         Assert.AreEqual(imcWaited,imc);
     }
 }
